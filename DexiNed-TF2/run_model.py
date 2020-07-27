0861b7e00ec906cdf577aa8b3757643b787af172
Ranajit Hore <62388874+ranajit1996@users.noreply.github.com>
2020-07-27 23:38:03 +0530

    Update run_model.py

--- DexiNed-TF2/run_model.py
+++ DexiNed-TF2/run_model.py
@@ -160,7 +160,7 @@ class run_DexiNed():
                                      self.args.model_name + "2" + self.args.data4train)
 
         my_model.load_weights(os.path.join(checkpoit_dir, "DexiNed23_model.h5"))
-        my_model.save("dexined.h5", include_optimizer=False)
+        my_model.save("dexined", include_optimizer=False)
         
 
         result_dir = os.path.join(
